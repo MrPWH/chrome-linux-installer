1d9b547922214e7eebbb38a5414ed751afa6cd13
MR.PWH <89309074+MrPWH@users.noreply.github.com>
2023-02-10 16:11:12 +0330

    Add files via upload

---
+++ install-google-chrome.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install wget
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install ./google-chrome-stable_current_amd64.deb
+echo '\nGoogle Chrome Browser iS Installed!'
